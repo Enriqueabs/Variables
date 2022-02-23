f401dc32fccf1adca191b9c24b0a1a6e297c3edb
enrique becerra <enriquealejandrobs@hotmail.com>
2022-02-22 19:10:17 -0800

    perimetro

---
+++ Calculo Perimetro/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Perimetro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            int a = 10;
+            int b = 10;
+            int c = 7;
+            int p = a + b + c;
+            Console.WriteLine("el perimetro del triangulo es:" + p);
+            Console.WriteLine("presione cuakquier tecla para finalizar");
+            Console.ReadKey();
+        }
+
+        //
+
+        //
+    }
+    }
+}
